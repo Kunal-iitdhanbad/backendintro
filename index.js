55b677f2eb4c68a5ce64e9ffd0d1dbb66ff82844
Kunal-iitdhanbad <kunalkumarr05@gmail.com>
2024-11-30 16:42:48 +0530

    aise hi kuch data add kiye h

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
+const githubdata={
+  "message":"API rate limit exceeded for 117.250.76.237. (But here's the good news: Authenticated requests get a higher rate limit. Check out the documentation for more details.)","documentation_url":"https://docs.github.com/rest/overview/resources-in-the-rest-api#rate-limiting"}
+
+
 app.get('/', (req, res) => {
   res.send("kya baba");
 });
@@ -10,6 +14,9 @@ app.get('/', (req, res) => {
 app.get('/kuchsikha', (req,res) => {
   res.send("kya sikha backend");
 });
+app.get('/github',(req,res)=>{
+  res.json(githubdata)
+})
 
 app.listen(process.env.port, () => {
   console.log("first project in backend");
